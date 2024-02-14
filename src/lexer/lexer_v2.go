b15046f246e96256de6a0c9280ff295f31c3ed8a
SpandanBG <spandanbg@gmail.com>
2024-02-15 00:00:09 +0530

    update new lexer (v2) to handle more keywords

--- src/lexer/lexer_v2.go
+++ src/lexer/lexer_v2.go
@@ -2,7 +2,9 @@ package lexer
 
 import (
 	"bufio"
+	"bytes"
 	"io"
+	"unicode"
 
 	"sudocoding.xyz/interpreter_in_go/src/token"
 )
@@ -19,11 +21,13 @@ func New_V2(reader io.Reader) *Lexer_V2 {
 }
 
 func (l_v2 *Lexer_V2) NextToken_V2() token.Token {
-  var tok token.Token
+	var tok token.Token
 
-  switch l_v2.ch {
-  case rune('='):
-    tok = token.Token{Type: token.ASSIGN, Literal: string(l_v2.ch)}
+	l_v2.skipWhitespace()
+
+	switch l_v2.ch {
+	case rune('='):
+		tok = token.Token{Type: token.ASSIGN, Literal: string(l_v2.ch)}
 	case rune('+'):
 		tok = token.Token{Type: token.PLUS, Literal: string(l_v2.ch)}
 	case rune('('):
@@ -38,12 +42,24 @@ func (l_v2 *Lexer_V2) NextToken_V2() token.Token {
 		tok = token.Token{Type: token.COMMA, Literal: string(l_v2.ch)}
 	case rune(';'):
 		tok = token.Token{Type: token.SEMICOLON, Literal: string(l_v2.ch)}
-  case rune(0):
-    tok = token.Token{Type: token.EOF, Literal: string(l_v2.ch)}
-  }
+	case rune(0):
+		tok = token.Token{Type: token.EOF, Literal: string(l_v2.ch)}
+	default:
+		if isLetter_v2(l_v2.ch) {
+			tok.Literal = l_v2.readGroup_v2(isLetter_v2)
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		} else if isDigit_v2(l_v2.ch) {
+			tok.Literal = l_v2.readGroup_v2(isDigit_v2)
+			tok.Type = token.INT 
+			return tok
+		} else {
+			tok = token.Token{Type: token.ILLEGAL, Literal: string(l_v2.ch)}
+		}
+	}
 
-  l_v2.readChar_v2()
-  return tok
+	l_v2.readChar_v2()
+	return tok
 }
 
 func (l_v2 *Lexer_V2) readChar_v2() {
@@ -53,4 +69,29 @@ func (l_v2 *Lexer_V2) readChar_v2() {
 	}
 
 	l_v2.ch = ch
+}
+
+func (l_v2 *Lexer_V2) readGroup_v2(filterFn func(ch rune) bool) string {
+	var idBuffer bytes.Buffer
+
+	for filterFn(l_v2.ch) {
+		idBuffer.WriteRune(l_v2.ch)
+		l_v2.readChar_v2()
+	}
+
+	return idBuffer.String()
+}
+
+func (l_v2 *Lexer_V2) skipWhitespace() {
+	for unicode.IsSpace(l_v2.ch) {
+		l_v2.readChar_v2()
+	}
+}
+
+func isLetter_v2(ch rune) bool {
+	return unicode.IsLetter(ch) || ch == rune('_')
+}
+
+func isDigit_v2(ch rune) bool {
+	return unicode.IsDigit(ch)
 }
--- src/lexer/lexer_v2_test.go
+++ src/lexer/lexer_v2_test.go
@@ -7,7 +7,7 @@ import (
 	"sudocoding.xyz/interpreter_in_go/src/token"
 )
 
-func Test_New_V2(t *testing.T) {
+func Test_BaseNew_V2(t *testing.T) {
 	input := strings.NewReader("=+(){},;")
 
 	expectedTokens := []token.Token{
@@ -26,6 +26,70 @@ func Test_New_V2(t *testing.T) {
 
 	for i, expectedToken := range expectedTokens {
 		tok := lexer_v2.NextToken_V2()
+
+		if tok.Type != expectedToken.Type {
+			t.Fatalf("test[%d] - tokentype wrong. expected=%q, got=%q", i, expectedToken.Type, tok.Type)
+		}
+
+		if tok.Literal != expectedToken.Literal {
+			t.Fatalf("test[%d] - literal wrong. expected=%q, got=%q", i, expectedToken.Literal, tok.Literal)
+		}
+	}
+}
+
+func Test_NextToken_V2(t *testing.T) {
+	input := strings.NewReader(`let five = 5;
+  let ten=10;
+
+  let add = fn(x, y){
+    x + y;
+  }
+
+  let result = add(five, ten);`)
+
+	expectedTokens := []token.Token{
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "five"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.INT, Literal: "5"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "ten"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.INT, Literal: "10"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "add"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.FUNCTION, Literal: "fn"},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.IDENT, Literal: "y"},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.LBRACE, Literal: "{"},
+		{Type: token.IDENT, Literal: "x"},
+		{Type: token.PLUS, Literal: "+"},
+		{Type: token.IDENT, Literal: "y"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.RBRACE, Literal: "}"},
+		{Type: token.LET, Literal: "let"},
+		{Type: token.IDENT, Literal: "result"},
+		{Type: token.ASSIGN, Literal: "="},
+		{Type: token.IDENT, Literal: "add"},
+		{Type: token.LPAREN, Literal: "("},
+		{Type: token.IDENT, Literal: "five"},
+		{Type: token.COMMA, Literal: ","},
+		{Type: token.IDENT, Literal: "ten"},
+		{Type: token.RPAREN, Literal: ")"},
+		{Type: token.SEMICOLON, Literal: ";"},
+		{Type: token.EOF, Literal: "\x00"},
+	}
+
+	lexer := New_V2(input)
+
+	for i, expectedToken := range expectedTokens {
+		tok := lexer.NextToken_V2()
 
 		if tok.Type != expectedToken.Type {
 			t.Fatalf("test[%d] - tokentype wrong. expected=%q, got=%q", i, expectedToken.Type, tok.Type)
