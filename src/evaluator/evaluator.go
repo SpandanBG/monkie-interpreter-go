aa7a3f3db58b068933ca43aaac5f096e52f19151
SpandanBG <spandanbg@gmail.com>
2024-03-06 22:35:35 +0530

    Add integer literal evaluator

---
+++ src/evaluator/evaluator.go
@@ -0,0 +1,32 @@
+package evaluator
+
+import (
+	"sudocoding.xyz/interpreter_in_go/src/object"
+	"sudocoding.xyz/interpreter_in_go/src/parser/ast"
+)
+
+func Eval(node ast.Node) object.Object {
+	switch node := node.(type) {
+	// Statements
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+
+		// Expression
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	}
+
+	return nil
+}
+
+func evalStatements(statements []ast.Statement) object.Object {
+	var result object.Object
+
+	for _, statement := range statements {
+		result = Eval(statement)
+	}
+
+	return result
+}
---
+++ src/evaluator/evaluator_test.go
@@ -0,0 +1,53 @@
+package evaluator
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"sudocoding.xyz/interpreter_in_go/src/lexer"
+	"sudocoding.xyz/interpreter_in_go/src/object"
+	"sudocoding.xyz/interpreter_in_go/src/parser"
+)
+
+func eq[T comparable](t *testing.T, expected T, actual T, msg ...string) {
+	if expected != actual {
+		t.Fatalf("%s\nexpected: %+v\nactual: %+v\n", strings.Join(msg, " "), expected, actual)
+	}
+}
+
+func notEq[T comparable](t *testing.T, expected T, actual T, msg ...string) {
+	if expected == actual {
+		t.Fatalf("%s\nexpected not: %+v\nactual: %+v\n", strings.Join(msg, " "), expected, actual)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.New_V2(strings.NewReader(input))
+	p := parser.New(l)
+	program := p.ParseProgram()
+
+	return Eval(program)
+}
+
+func testIntegerObj(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	eq(t, true, ok, "Failed to typecast obj to object.Integer")
+	eq(t, expected, result.Value, "Expected int64 didn't match")
+	return true
+}
+
+func Test_EvalIntegerExpression(t *testing.T) {
+	for _, test := range []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10},
+	} {
+		t.Run(fmt.Sprintf("Tests for %s", test.input), func(t *testing.T) {
+			evaluated := testEval(test.input)
+			eq(t, true, testIntegerObj(t, evaluated, test.expected), "Did not match expected")
+		})
+	}
+}
